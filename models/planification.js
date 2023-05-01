7307de25d4088bb9c9ec0e0c347cd771c16b8665
daniellfreelancer <daniel.jlugo@gmail.com>
2023-05-01 11:40:33 -0300

    new model for planification

--- models/planification.js
+++ models/planification.js
@@ -4,17 +4,19 @@ const mongoose = require('mongoose')
 const planificationSchema = new mongoose.Schema({
 
     classroom: {type: mongoose.Types.ObjectId, ref:'classroom'},
-    date: { type: String, required: true },
+    startDate: { type: String, required: true },
+    endDate: { type: String },
     duration: { type: Number, required: true },
-    classObjectives: { type: String, required: true },
-    learningObjectives: { type: String, required: true },
-    evaluationIndicators: { type: String, required: true },
-    skills: { type: String, required: true },
-    activities: { type: String, required: true },
-    materials: { type: String, required: true },
-    evaluationType: { type: String, required: true },
+    schoolBlock: { type: Number, required: true },
     content: { type: String, required: true },
-    
+    classObjectives: { type: Array, required: true },
+    evaluationIndicators: { type: Array, required: true },
+    evaluationIndicatorsTeacher: { type: Array, required: true },
+    learningObjectives: { type: Array, required: true },
+    activities: { type: String, required: true },
+    materials: { type: Array, required: true },
+    evaluationType: { type: String, required: true },
+   
   }, { timestamps: true });
   
 
