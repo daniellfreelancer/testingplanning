d8b7597dfa9dcabf385c4e4dcd14748cd5e8b22e
daniellfreelancer <daniel.jlugo@gmail.com>
2023-06-07 18:37:23 -0300

    update file and password for student

--- controllers/planificationController.js
+++ controllers/planificationController.js
@@ -6,50 +6,92 @@ const ClassRoom = require('../models/classroom')
 const planificationController = {
 
 
+    // createPlanification: async (req, res) => {
+    //   // let {filename} = req.file
+    //     try {
+    //       const newPlanification = await new Planification(req.body).save();
+
+    //       if (req.file){
+    //         let {filename} = req.file
+    //         newPlanification.quiz = filename
+
+    //         await newPlanification.save()
+    //       }
+
+
+      
+    //       if (newPlanification) {
+    //         let planificationID = newPlanification._id;
+      
+    //         const classroomGrade = await ClassRoom.findById(req.body.classroom);
+      
+    //         if (classroomGrade) {
+    //           classroomGrade.planner.push(planificationID);
+    //           const updatedClassroomGrade = await classroomGrade.save();
+      
+    //           res.status(200).json({
+    //             response: newPlanification,
+    //             id: planificationID,
+    //             message: "Planificación creada y agregada al aula",
+    //           });
+    //         }
+    //       } else {
+    //         res.status(400).json({
+    //           message: "Error al crear la planificación",
+    //           success: false,
+    //         });
+    //       }
+    //     } catch (error) {
+    //       console.log(error);
+    //       res.status(400).json({
+    //         message: "Error al intentar crear la planificación",
+    //         success: false,
+    //       });
+    //     }
+    //   },
+     
     createPlanification: async (req, res) => {
-      // let {filename} = req.file
-        try {
-          const newPlanification = await new Planification(req.body).save();
-
-          if (req.file){
-            let {filename} = req.file
-            newPlanification.quiz = filename
-
-            await newPlanification.save()
-          }
-
-
-      
-          if (newPlanification) {
-            let planificationID = newPlanification._id;
-      
-            const classroomGrade = await ClassRoom.findById(req.body.classroom);
-      
-            if (classroomGrade) {
-              classroomGrade.planner.push(planificationID);
-              const updatedClassroomGrade = await classroomGrade.save();
-      
-              res.status(200).json({
-                response: newPlanification,
-                id: planificationID,
-                message: "Planificación creada y agregada al aula",
-              });
-            }
-          } else {
-            res.status(400).json({
-              message: "Error al crear la planificación",
-              success: false,
+      try {
+        const newPlanification = new Planification(req.body);
+    
+        if (req.file) {
+          let { filename } = req.file;
+          newPlanification.quiz = filename;
+        }
+    
+        const savedPlanification = await newPlanification.save();
+    
+        if (savedPlanification) {
+          const planificationID = savedPlanification._id;
+    
+          const classroomGrade = await ClassRoom.findById(req.body.classroom);
+    
+          if (classroomGrade) {
+            classroomGrade.planner.push(planificationID);
+            const updatedClassroomGrade = await classroomGrade.save();
+    
+            res.status(200).json({
+              response: savedPlanification,
+              id: planificationID,
+              message: "Planificación creada y agregada al aula",
             });
           }
-        } catch (error) {
-          console.log(error);
+        } else {
           res.status(400).json({
-            message: "Error al intentar crear la planificación",
+            message: "Error al crear la planificación",
             success: false,
           });
         }
-      },
-      deletePlanification : async (req, res) => {
+      } catch (error) {
+        console.log(error);
+        res.status(400).json({
+          message: "Error al intentar crear la planificación",
+          success: false,
+        });
+      }
+    },
+    
+    deletePlanification : async (req, res) => {
         const { planificationId, classroomId } = req.params;
         try {
           const planification = await Planification.findById(planificationId);
