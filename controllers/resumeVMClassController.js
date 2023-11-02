83cef6eecc56284913ab4c67f0bfe15c8087e077
Daniel Lugo <daniellugofreelancer@gmail.com>
2023-11-02 09:22:30 -0300

    actualizacion #2 para subir a servidor@

--- controllers/resumeVMClassController.js
+++ controllers/resumeVMClassController.js
@@ -43,42 +43,6 @@ const createSurveysForStudents = async (resumeId, arrayStudentsForSurvey, classr
 
 const resumeVMClassController = {
 
-
-
-
-  //     try {
-
-  //         let resume = await new ResumeVmClass(req.body).save()
-
-  //         // Verificar si se cargó la imagen del campo imgFirstVMClass
-  //         if (req.files && req.files['imgFirstVMClass']) {
-  //             const imgFirstVMClass = req.files['imgFirstVMClass'][0];
-  //             resume.setImgFirstVMClassUrl(imgFirstVMClass.filename);
-  //         }
-
-  //         // Verificar si se cargó la imagen del campo imgSecondVMClass
-  //         if (req.files && req.files['imgSecondVMClass']) {
-  //             const imgSecondVMClass = req.files['imgSecondVMClass'][0];
-  //             resume.setImgSecondVMClassUrl(imgSecondVMClass.filename);
-  //         }
-
-  //         // Verificar si se cargó la imagen del campo imgThirdVMClass
-  //         if (req.files && req.files['imgThirdVMClass']) {
-  //             const imgThirdVMClass = req.files['imgThirdVMClass'][0];
-  //             resume.setImgThirdVMClassUrl(imgThirdVMClass.filename);
-  //         }
-
-  //         // Guardar la instancia del modelo en la base de datos
-  //         await resume.save();
-
-  //         res.status(200).json({ message: 'Resume VM added successfully' });
-  //     } catch (error) {
-  //         console.log(error)
-  //         res.status(500).json({ error: 'Failed to add Resume VM' });
-  //     }
-
-
-  // } 
   createResume: async (req, res) => {
 
     const clientAWS = new S3Client({
