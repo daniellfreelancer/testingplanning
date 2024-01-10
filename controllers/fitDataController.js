857aed2e1344c7e520ff57d953f94f0c3e0a615f
Daniel Lugo <daniellugofreelancer@gmail.com>
2024-01-10 18:15:51 -0300

    update fitdata register

--- controllers/fitDataController.js
+++ controllers/fitDataController.js
@@ -1,15 +1,20 @@
 const FitDataGoogle = require('../models/fitData')
 const Users = require('../models/admin')
 const Students = require('../models/student')
-
+const mongoose = require('mongoose');
 
 
 const queryPopulateUser = [
     {
-        path: 'user student',
-        select: 'name lastName age size weight role gender'
-    }
-]
+        path: 'user',
+        select: 'name lastName age size weight role gender',
+    },
+    {
+        path: 'student',
+        select: 'name lastName age size weight role gender',
+    },
+];
+
 
 
 const fitDataController = {
@@ -43,8 +48,9 @@ const fitDataController = {
 
                 userData.fitData.push(fitDataUser._id)
                 await userData.save()
+
             } else {
-                const userData  = await Students.findById(student)
+                const userData = await Students.findById(student)
 
                 if (!userData) {
                     return res.status(401).json(
@@ -57,27 +63,115 @@ const fitDataController = {
                 await userData.save()
             }
 
-                if (fitDataUser) {
-                    res.status(200).json({
-                        response: fitDataUser,
-                        message: "Se ha registrado correctamente la data",
-                        success: true
-                    })
-                } else {
-                    res.status(404).json({
-                        error: "Error al guardar los datos",
-                        success: false
-                    })
-                }
-
-
-
-            } catch (error) {
-                console.error(error)
-                res.status(500).json({ error: 'Error al registrar la actividad' })
+            if (fitDataUser) {
+                res.status(200).json({
+                    response: fitDataUser,
+                    message: "Se ha registrado correctamente la data",
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    error: "Error al guardar los datos",
+                    success: false
+                })
             }
 
+
+
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({ error: 'Error al registrar la actividad' })
         }
+
+    },
+
+    getFitDataByUser: async (req, res) => {
+
+        try {
+
+            const { id } = req.params
+            const fitData = await FitDataGoogle.find({ user: id }).populate(queryPopulateUser)
+            if (fitData) {
+                res.status(200).json({
+                    response: fitData,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    error: "No se ha encontrado la data",
+                    success: false
+                })
+            }
+
+        } catch (error) {
+
+        }
+
+    },
+    getFitData: async (req, res) => {
+
+
+        try {
+
+            const fitData = await FitDataGoogle.find()
+            .populate(queryPopulateUser[0])  // Primera llamada a populate para 'user'
+            .populate(queryPopulateUser[1]); 
+
+            if (fitData) {
+                res.status(200).json({
+                    response: fitData,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    error: "No se ha encontrado la data",
+                    success: false
+                })
+            }
+
+        } catch (error) {
+
+            console.error(error)
+            res.status(500).json({ error: 'Error al intentar obtener la data' })
+
+        }
+
+
+    },
+
+    updateFitData: async (req, res) => {
+
+        try {
+            const { user, student, ...fitData } = req.body;
+            let query = user ? { user, createdAt: { $gte: new Date().setHours(0, 0, 0, 0), $lt: new Date().setHours(23, 59, 59, 999) } }
+                : { student, createdAt: { $gte: new Date().setHours(0, 0, 0, 0), $lt: new Date().setHours(23, 59, 59, 999) } };
+
+            const existingFitData = await FitDataGoogle.findOne(query);
+
+            if (existingFitData) {
+                // Actualizar el registro existente
+                Object.assign(existingFitData, fitData);
+                await existingFitData.save();
+                res.status(200).json({ response: existingFitData, message: "Data actualizada correctamente", success: true });
+            } else {
+                // No existe un registro para hoy, crear uno nuevo
+                const fitDataUser = new FitDataGoogle({ ...fitData, user, student });
+                await fitDataUser.save();
+                res.status(200).json({ response: fitDataUser, message: "Data registrada correctamente", success: true });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error al procesar la solicitud' });
+        }
+
     }
+
+
+
+
+
+
+
+}
 
 module.exports = fitDataController
--- routes/fitData.routes.js
+++ routes/fitData.routes.js
@@ -1,7 +1,10 @@
 var express = require('express');
-const { registerFitData } = require('../controllers/fitDataController');
+const { registerFitData, getFitData, updateFitData } = require('../controllers/fitDataController');
 var router = express.Router();
 
 router.post('/register', registerFitData )
+router.get('/get-data', getFitData)
+router.post('/update', updateFitData);  // Nueva ruta para actualizar los datos
+
 
 module.exports = router;
