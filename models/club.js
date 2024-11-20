9ffa94c608b253eadb767dafbbad35f7f351d5d3
Daniel L <daniellugofreelancer@gmail.com>
2024-11-20 13:02:14 -0300

    crear club modelo

--- models/club.js
+++ models/club.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const clubSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    location: { type: String, required: true },
+    address: { type: String, required: true },
     logo: { type: String },
     email: {type: String,required: true},  
     phone: {type: String, required: true},
