7841803246925cd417d55bdfb77b46e2caac5be8
Daniel L <daniellugofreelancer@gmail.com>
2024-11-21 13:36:22 -0300

    club controller update

--- routes/clubes.routes.js
+++ routes/clubes.routes.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();const upload = require('../libs/docsStorage');
-const { createClub, getClubs, getClubById, updateClubLogo, deleteClub } = require('../controllers/clubController')
+const { createClub, getClubs, getClubById, updateClubLogo, deleteClub, getClubByInstitution } = require('../controllers/clubController')
 
 router.post('/create-club/:institutionId', createClub)
 router.get('/get-clubs', getClubs)
 router.get('/get-club/:id', getClubById)
+router.get('/get-clubs-institution/:institutionId', getClubByInstitution)
 router.patch('/update-club-logo/:id', upload.single('logo'), updateClubLogo)
 router.delete('/delete-club/:clubId/institution/:institutionId', deleteClub)
 
