fc7899bb264cdc13a236c81cbf2f336a53d2c268
Daniel L <rodrigo.viera.c@gmail.com>
2025-06-06 11:23:16 -0300

    add - profile user rekognition photo

--- controllers/aws.js
+++ controllers/aws.js
@@ -55,7 +55,6 @@ const rekoController = {
       const command = new SearchFacesByImageCommand(params);
       const data = await rekognitionClient.send(command);
 
-
       const match = data?.FaceMatches?.[0];
 
       if (!match) {
@@ -69,13 +68,13 @@ const rekoController = {
       const rut = externalImageId.split(".")[0];
 
       // Buscar en ambas colecciones
-    //   const user = await USERS.findOne({ rut });
-    //   const student = await STUDENTS.findOne({ rut });
+      //   const user = await USERS.findOne({ rut });
+      //   const student = await STUDENTS.findOne({ rut });
 
-        const [user, student] = await Promise.all([
-      USERS.findOne({ rut }),
-      STUDENTS.findOne({ rut }),
-    ]);
+      const [user, student] = await Promise.all([
+        USERS.findOne({ rut }),
+        STUDENTS.findOne({ rut }),
+      ]);
 
       if (!user && !student) {
         return res.status(404).json({
@@ -299,6 +298,158 @@ const rekoController = {
           success: true,
           data: newStudent,
         });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: error.message,
+        success: false,
+      });
+    }
+  },
+  subirFotoUsuarioRegistrado: async (req, res) => {
+    const { rut } = req.body;
+
+    try {
+      const [user, student] = await Promise.all([
+        USERS.findOne({ rut }),
+        STUDENTS.findOne({ rut }),
+      ]);
+
+      if (!user && !student) {
+        return res.status(404).json({
+          message: "Usuario no encontrado",
+          success: false,
+        });
+      }
+
+      if (user) {
+        if (req.file) {
+          const fileContent = req.file.buffer;
+          const extension = req.file.originalname.split(".").pop();
+          const fileName = `${rut}.${extension}`;
+
+          //foto para s# rekognition
+          const uploadParamsReko = {
+            Bucket: process.env.AWS_BUCKET_NAME_REKO,
+            Key: fileName,
+            Body: fileContent,
+          };
+
+          // Subir el archivo a S3
+          const uploadCommandRekognition = new PutObjectCommand(
+            uploadParamsReko
+          );
+          await clientAWSRekognition.send(uploadCommandRekognition);
+
+          const indexParams = {
+            CollectionId: process.env.COLLECTION_AWS, // Ej: "reconocimiento-facial"
+            Image: {
+              S3Object: {
+                Bucket: process.env.AWS_BUCKET_NAME_REKO,
+                Name: fileName,
+              },
+            },
+            ExternalImageId: fileName, // Puedes usar rut o nombre como identificador
+            DetectionAttributes: [],
+          };
+
+          try {
+            const indexCommand = new IndexFacesCommand(indexParams);
+            const indexResponse = await rekognitionClient.send(indexCommand);
+            console.log("✅ Rostro indexado en Rekognition:", indexResponse);
+
+            //update user
+            const userUpdate = await USERS.findOneAndUpdate(
+              { rut },
+              { new: true }
+            );
+
+            userUpdate.imgRekognition = fileName;
+
+            await userUpdate.save();
+
+            res.status(200).json({
+              message: "Foto actualizada con exito",
+              success: true,
+              data: userUpdate,
+            });
+          } catch (indexError) {
+            console.error(
+              "❌ Error al indexar rostro en Rekognition:",
+              indexError
+            );
+            return res.status(500).json({
+              message: "Error al indexar rostro en Rekognition",
+              details: indexError.message || indexError,
+            });
+          }
+        }
+      } else if (student) {
+        if (req.file) {
+          const fileContent = req.file.buffer;
+          const extension = req.file.originalname.split(".").pop();
+          const fileName = `${rut}.${extension}`;
+
+          //foto para s# rekognition
+          const uploadParamsReko = {
+            Bucket: process.env.AWS_BUCKET_NAME_REKO,
+            Key: fileName,
+            Body: fileContent,
+          };
+
+          // Subir el archivo a S3
+          const uploadCommandProfile = new PutObjectCommand(uploadParams);
+          await clientAWSProfile.send(uploadCommandProfile);
+
+          const uploadCommandRekognition = new PutObjectCommand(
+            uploadParamsReko
+          );
+          await clientAWSRekognition.send(uploadCommandRekognition);
+
+          const indexParams = {
+            CollectionId: process.env.COLLECTION_AWS, // Ej: "reconocimiento-facial"
+            Image: {
+              S3Object: {
+                Bucket: process.env.AWS_BUCKET_NAME_REKO,
+                Name: fileName,
+              },
+            },
+            ExternalImageId: fileName, // Puedes usar rut o nombre como identificador
+            DetectionAttributes: [],
+          };
+
+          try {
+            const indexCommand = new IndexFacesCommand(indexParams);
+            const indexResponse = await rekognitionClient.send(indexCommand);
+            console.log("✅ Rostro indexado en Rekognition:", indexResponse);
+
+            //update user
+            const studentUpdate = await STUDENTS.findOneAndUpdate(
+              { rut },
+              { new: true }
+            );
+
+            studentUpdate.imgRekognition = fileName;
+
+            await studentUpdate.save();
+
+            res.status(200).json({
+              message: "Foto actualizada con exito",
+              success: true,
+              data: studentUpdate,
+            });
+          } catch (indexError) {
+            console.error(
+              "❌ Error al indexar rostro en Rekognition:",
+              indexError
+            );
+            return res.status(500).json({
+              message: "Error al indexar rostro en Rekognition",
+              details: indexError.message || indexError,
+            });
+          }
+        }
       }
     } catch (error) {
       console.log(error);
