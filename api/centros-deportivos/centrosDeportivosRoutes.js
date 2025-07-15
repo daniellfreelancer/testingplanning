174132bd05118f44992d6aba8839fd0b5e004ad8
Daniel L <rodrigo.viera.c@gmail.com>
2025-07-15 16:49:18 -0300

    ultimos cambios centros deportivos

--- api/centros-deportivos/centrosDeportivosRoutes.js
+++ api/centros-deportivos/centrosDeportivosRoutes.js
@@ -6,10 +6,10 @@ router.post('/crear-centro-deportivo/:id', centrosDeportivosController.crearCent
 router.put('/actualizar-centro-deportivo/:id', centrosDeportivosController.actualizarCentroDeportivo); //tested
 router.get('/obtener-centro-deportivo/:id', centrosDeportivosController.obtenerCentroDeportivo); //tested
 router.get('/obtener-todos-los-centros-deportivos', centrosDeportivosController.obtenerTodosLosCentrosDeportivos); //tested
-router.post('/agregar-admin-a-centro-deportivo/:id', centrosDeportivosController.agregarAdminACentroDeportivo);
-router.post('/eliminar-admin-de-centro-deportivo/:id', centrosDeportivosController.eliminarAdminDeCentroDeportivo);
-router.post('/agregar-institucion-a-centro-deportivo/:id', centrosDeportivosController.agregarInstitucionACentroDeportivo);
-router.post('/eliminar-institucion-de-centro-deportivo/:id', centrosDeportivosController.eliminarInstitucionDeCentroDeportivo);
+router.post('/agregar-admin-a-centro-deportivo/:id', centrosDeportivosController.agregarAdminACentroDeportivo);// tested
+router.post('/eliminar-admin-de-centro-deportivo/:id', centrosDeportivosController.eliminarAdminDeCentroDeportivo); //tested
+router.post('/agregar-institucion-a-centro-deportivo/:id', centrosDeportivosController.agregarInstitucionACentroDeportivo);//tested
+router.post('/eliminar-institucion-de-centro-deportivo/:id', centrosDeportivosController.eliminarInstitucionDeCentroDeportivo);//tested
 router.delete('/eliminar-centro-deportivo/:id', centrosDeportivosController.eliminarCentroDeportivo);
 
 module.exports = router;
