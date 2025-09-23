6ff5515d1a64773b811e6db82a9ac8e2feac9cf5
Daniel Lugo <daniellugofreelancer@gmail.com>
2025-09-23 10:54:38 -0300

    update gestion de plantes schema

--- api/gestion-planes/gestionPlanes.js
+++ api/gestion-planes/gestionPlanes.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const gestionPlanesSchema = new mongoose.Schema({
-    tipo: { type: String, required: true },
+    tipo: { type: String, required: true,
+        enum: ['curso', 'nadoLibre', 'gimnasio']
+     },
     tipoPlan: { type: String, required: true },
     plan: { type: String, required: true },
-    dias: { type: Array, required: true },
-    horarios: { type: Array, required: true },
+    tieneVariante: { type: Boolean, required: true },
+    variante:{ type: Object },
+    dias: { type: Array, },
+    horarios: { type: Array,},
     valor: { type: Number, required: true },
     institucion: { type: mongoose.Types.ObjectId, ref: 'institucion', required: true },
     usuarios: [{ type: mongoose.Types.ObjectId, ref: 'usuariosComplejos' }],
@@ -17,3 +21,35 @@ const gestionPlanesSchema = new mongoose.Schema({
 const GestionPlanes = mongoose.model('gestionPlanes', gestionPlanesSchema);
 
 module.exports = GestionPlanes
+
+
+/**
+ * tipo: 'curso',
+ * tipoPlan: 'Matronatación',
+ * plan: '2 veces por semana',
+ * tieneVariante: true,
+ * variante:{
+ *  nombre:'Sabado',
+ *  dias:['Sabado'],
+ *  horarios:['09:00']},
+ * dias:null,
+ * horarios:null,
+ * valor: 35000,
+ * institucion: 'ObjectId',
+ * usuarios: ['ObjectId1','ObjectId2'],
+ * status: true
+ */
+
+/**
+ * tipo:"curso",
+ * tipoPlan:"Curso adulto 1",
+ * plan:'1 vez por semana',
+ * tieneVariante:false,
+ * variante:null,
+ * dias:['sabado'],
+ * horarios:['13:00'],
+ * valor:30000,
+ * institucion:"ObjectId",
+ * usuarios:[],
+ * status:true
+ */
