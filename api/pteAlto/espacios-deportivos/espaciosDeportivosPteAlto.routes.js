2319adb2f001b76cff5c312efabbd28bd2b245f3
Matias Guzman <bluxterz@MacBook-Air-de-Matias.local>
2025-12-03 10:27:00 -0300

    puente alto

--- api/pteAlto/espacios-deportivos/espaciosDeportivosPteAlto.routes.js
+++ api/pteAlto/espacios-deportivos/espaciosDeportivosPteAlto.routes.js
@@ -6,7 +6,7 @@ const upload = require('../../../libs/docsStorage');
 router.post('/crear-espacio/:complejoDeportivo', upload.single('imgUrl'), espaciosDeportivosPteAltoController.crearEspacioDeportivoPteAlto);
 router.get('/obtener-todos-los-espacios', espaciosDeportivosPteAltoController.obtenerTodosLosEspaciosDeportivosPteAlto);
 router.get('/obtener-espacio/:id', espaciosDeportivosPteAltoController.obtenerEspacioDeportivoPteAltoPorId);
-router.put('/actualizar-espacio/:id', espaciosDeportivosPteAltoController.actualizarEspacioDeportivoPteAltoPorId);
+router.put('/actualizar-espacio/:id', upload.single('imgUrl'), espaciosDeportivosPteAltoController.actualizarEspacioDeportivoPteAltoPorId);
 router.delete('/eliminar-espacio/:id', espaciosDeportivosPteAltoController.eliminarEspacioDeportivoPteAltoPorId);
 
 module.exports = router;
