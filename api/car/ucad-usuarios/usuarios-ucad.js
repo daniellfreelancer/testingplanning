895d10b7b16d4e4f5f17bb3d82877e92c63d0532
Daniel Lugo <rodrigo.viera.c@gmail.com>
2025-12-17 16:31:30 -0300

    fix indexado de datos de los usuarios

--- api/car/ucad-usuarios/usuarios-ucad.js
+++ api/car/ucad-usuarios/usuarios-ucad.js
@@ -22,17 +22,3 @@ const usuariosUcadSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("usuariosUcad", usuariosUcadSchema);
-
-usuariosUcadSchema.index({ email: 1 }, { unique: true });
-usuariosUcadSchema.index({ rut: 1 }, { unique: true });
-usuariosUcadSchema.index({ telefono: 1 }, { unique: true });
-usuariosUcadSchema.index({ estadoValidacion: 1 });
-usuariosUcadSchema.index({ especialidad: 1 });
-usuariosUcadSchema.index({ rol: 1 });
-usuariosUcadSchema.index({ fechaNacimiento: 1 });
-usuariosUcadSchema.index({ sexo: 1 });
-usuariosUcadSchema.index({ comuna: 1 });
-usuariosUcadSchema.index({ region: 1 });
-usuariosUcadSchema.index({ direccion: 1 });
-usuariosUcadSchema.index({ imgUrl: 1 });
-usuariosUcadSchema.index({ fechaRegistro: 1 });
