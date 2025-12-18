c1c0e7b29c12fd88875875bb3ce05e97cc5944d6
ShunZhouZ <tutizhou@gmail.com>
2025-12-18 12:48:00 -0300

    recuperar password funcional

--- api/car/ucad-usuarios/usuarios-ucad.routes.js
+++ api/car/ucad-usuarios/usuarios-ucad.routes.js
@@ -21,4 +21,7 @@ router.put("/editar-usuario/:_id", upload.single('imgUrl'), usuariosUcadControll
 // Eliminar usuario UCAD
 router.delete("/eliminar-usuario/:_id", usuariosUcadController.eliminarUsuarioUCAD);
 
+// Recuperar contraseña
+router.post("/recuperar-password", usuariosUcadController.recuperarPasswordUCAD);
+
 module.exports = router;
