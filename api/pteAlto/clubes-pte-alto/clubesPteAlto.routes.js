0a848086fc2512a011237fa91d7c0a382a57a780
Daniel Lugo <rodrigo.viera.c@gmail.com>
2026-02-05 13:25:31 -0300

    organizaciones pte alto

--- api/pteAlto/clubes-pte-alto/clubesPteAlto.routes.js
+++ api/pteAlto/clubes-pte-alto/clubesPteAlto.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../../../libs/docsStorage');
 const clubesPteAltoController = require('./clubesPteAltoController');
 
-router.post('/crear-club-pte-alto', clubesPteAltoController.crearClubPteAlto);
+router.post('/crear-club-pte-alto', upload.single('documento'), clubesPteAltoController.crearClubPteAlto);
 router.get('/obtener-todos-los-clubes-pte-alto', clubesPteAltoController.obtenerTodosLosClubesPteAlto);
 router.get('/obtener-club-pte-alto-por-id/:id', clubesPteAltoController.obtenerClubPteAltoPorId);
-router.put('/actualizar-club-pte-alto-por-id/:id', clubesPteAltoController.actualizarClubPteAltoPorId);
+router.put('/actualizar-club-pte-alto-por-id/:id', upload.single('documento'), clubesPteAltoController.actualizarClubPteAltoPorId);
 router.delete('/eliminar-club-pte-alto-por-id/:id', clubesPteAltoController.eliminarClubPteAltoPorId);
 
 module.exports = router;
